b2e279969d79149f676c74dea02b70e4664504a6
Gabriele Fronzoni <gabriele.fronzoni@studio.unibo.it>
2025-08-22 14:18:12 +0000

    Modified GCN model and removed a convolutional layer

--- GCNModel.py
+++ GCNModel.py
@@ -95,10 +95,10 @@ class DGCNN(nn.Module):
         self.conv5 = nn.Sequential(nn.Conv2d(256*2, 256, kernel_size=1, bias=False),
                                    self.bn5,
                                    nn.LeakyReLU(negative_slope=0.2))
-        self.conv6 = nn.Sequential(nn.Conv2d(256*2, 256, kernel_size=1, bias=False),
+        """"self.conv6 = nn.Sequential(nn.Conv2d(256*2, 256, kernel_size=1, bias=False),
                                    self.bn6,
-                                   nn.LeakyReLU(negative_slope=0.2))
-        self.conv7 = nn.Sequential(nn.Conv1d(1024, emb_dims, kernel_size=1, bias=False),
+                                   nn.LeakyReLU(negative_slope=0.2))""""
+        self.conv7 = nn.Sequential(nn.Conv1d(768, emb_dims, kernel_size=1, bias=False),
                                    self.bn7,
                                    nn.LeakyReLU(negative_slope=0.2))
         self.linear1 = nn.Linear(emb_dims*2, 512, bias=False)
@@ -138,11 +138,11 @@ class DGCNN(nn.Module):
         x = self.conv5(x)
         x5 = x.max(dim=-1, keepdim=False)[0]
 
-        x = get_graph_feature(x5, k=self.k)
+        """"x = get_graph_feature(x5, k=self.k)
         x = self.conv6(x)
-        x6 = x.max(dim=-1, keepdim=False)[0]
+        x6 = x.max(dim=-1, keepdim=False)[0]""""
 
-        x = torch.cat((x1, x2, x3, x4, x5, x6), dim=1)
+        x = torch.cat((x1, x2, x3, x4, x5), dim=1)
 
         x = self.conv7(x)
         x1 = F.adaptive_max_pool1d(x, 1).view(batch_size, -1)
